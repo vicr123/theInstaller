a6afe2f95af6fc3e9a993913de5d054d8f81a831
Victor Tran <vicr12345@gmail.com>
2018-10-06 12:17:12 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 100.0% (50 of 50 strings)
    
    Translation: theInstaller/theInstaller
    Translate-URL: http://translate.vicr123.com/projects/theinstaller/theinstaller/vi/

--- installer/translations/vi_VN.ts
+++ installer/translations/vi_VN.ts
@@ -1,195 +1,187 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="vi_VN">
 <context>
     <name>InstallWorker</name>
     <message>
         <location filename="../process/installworker.cpp" line="65"/>
         <location filename="../process/installworker.cpp" line="184"/>
         <source>Downloading %1...</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang tài xuống %1...</translation>
     </message>
     <message>
         <location filename="../process/installworker.cpp" line="88"/>
         <source>Unpacking %1...</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang giải nén %1...</translation>
     </message>
     <message>
         <location filename="../process/installworker.cpp" line="102"/>
         <source>Configuring %1...</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang cấu hình %1...</translation>
     </message>
     <message>
         <location filename="../process/installworker.cpp" line="193"/>
         <source>%1 of %2</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 trên %2</translation>
     </message>
     <message numerus="yes">
         <location filename="../process/installworker.cpp" line="205"/>
         <source>%n days remaining</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>Còn %n ngày nữa</numerusform>
+        </translation></message>
     <message numerus="yes">
         <location filename="../process/installworker.cpp" line="207"/>
         <source>%n hours remaining</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>Còn %n tiếng nữa</numerusform>
+        </translation></message>
     <message numerus="yes">
         <location filename="../process/installworker.cpp" line="209"/>
         <source>%n minutes remaining</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>Còn %n phút nữa</numerusform>
+        </translation></message>
     <message numerus="yes">
         <location filename="../process/installworker.cpp" line="211"/>
         <source>%n seconds remaining</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>Còn %n giây nữa</numerusform>
+        </translation></message>
 </context>
 <context>
     <name>MainWindow</name>
     <message>
         <location filename="../mainwindow.ui" line="60"/>
         <source>Getting ready...</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang chuẩn bị...</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="108"/>
         <source>This shouldn&apos;t be happening...</source>
-        <translation type="unfinished"></translation>
+        <translation>Điều này không nên xảy ra...</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="143"/>
         <source>Cancel Installation</source>
-        <translation type="unfinished"></translation>
+        <translation>Hủy cài đặt</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="150"/>
         <source>Retry</source>
-        <translation type="unfinished"></translation>
+        <translation>Thử lại</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="225"/>
         <location filename="../mainwindow.ui" line="481"/>
         <source>Install Now</source>
-        <translation type="unfinished"></translation>
+        <translation>Cài đặt ngay</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="262"/>
         <source>Install Options</source>
-        <translation type="unfinished"></translation>
+        <translation>Tùy chọn cài đặt</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="339"/>
         <source>Install To</source>
-        <translation type="unfinished"></translation>
+        <translation>Cài đặt trong</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="349"/>
         <source>Install for</source>
-        <translation type="unfinished"></translation>
+        <translation>Cài đặt cho</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="356"/>
         <source>Stream</source>
-        <translation type="unfinished"></translation>
+        <translation>Phiên bản phần mềm</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="363"/>
         <source>Browse...</source>
-        <translation type="unfinished"></translation>
+        <translation>Chọn...</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="391"/>
         <source>Everyone</source>
-        <translation type="unfinished"></translation>
+        <translation>Tất cả người dùng</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="407"/>
         <source>This user only</source>
-        <translation type="unfinished"></translation>
+        <translation>Chỉ người dùng này</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="438"/>
         <source>Stable</source>
-        <translation type="unfinished"></translation>
+        <translation>Bình thường</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="454"/>
         <source>Blueprint</source>
-        <translation type="unfinished"></translation>
+        <translation>Mới nhất</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="606"/>
         <source>Installation is complete.</source>
-        <translation type="unfinished"></translation>
+        <translation>Cài đặt đã xong.</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="668"/>
         <source>Finish</source>
-        <translation type="unfinished"></translation>
+        <translation>Kết thúc</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="723"/>
         <source>Installation failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Cài đặt đã thất bại.</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="748"/>
         <source>Exit</source>
-        <translation type="unfinished"></translation>
+        <translation>Thoát</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="755"/>
         <source>Try Again</source>
-        <translation type="unfinished"></translation>
+        <translation>Thử lại</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="39"/>
         <source>Invalid metadata was received</source>
-        <translation type="unfinished"></translation>
+        <translation>Siêu dữ liệu đã không nhận được thành công</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="46"/>
         <source>Application name not in metadata</source>
-        <translation type="unfinished"></translation>
+        <translation>Tên phần mềm không ở trong siêu dữ liệu</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="56"/>
         <source>Open %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Mở %1</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="57"/>
         <source>Install %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Cài đặt %1</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="65"/>
         <source>Couldn&apos;t retrieve metadata</source>
-        <translation type="unfinished"></translation>
+        <translation>Không nhận siêu dữ liệu được</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="69"/>
         <source>Couldn&apos;t connect securely to the server</source>
-        <translation type="unfinished"></translation>
+        <translation>Không kết nối an toàn đến máy chủ được</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="119"/>
         <source>Getting ready to install %1...</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang chuẩn bị cài đặt %1...</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="159"/>
         <source>Warning</source>
-        <translation type="unfinished"></translation>
+        <translation>Cảnh báo</translation>
     </message>
 </context>
 <context>
@@ -197,68 +189,68 @@
     <message>
         <location filename="../maintainwindow.ui" line="78"/>
         <source>Repair</source>
-        <translation type="unfinished"></translation>
+        <translation>Sửa</translation>
     </message>
     <message>
         <location filename="../maintainwindow.ui" line="85"/>
         <location filename="../maintainwindow.ui" line="172"/>
         <source>Uninstall</source>
-        <translation type="unfinished"></translation>
+        <translation>Gỡ cài đặt</translation>
     </message>
     <message>
         <location filename="../maintainwindow.ui" line="165"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Hủy</translation>
     </message>
     <message>
         <location filename="../maintainwindow.ui" line="293"/>
         <source>Removal is complete.</source>
-        <translation type="unfinished"></translation>
+        <translation>Việc gỡ cài đặt đã xong.</translation>
     </message>
     <message>
         <location filename="../maintainwindow.ui" line="318"/>
         <source>Finish</source>
-        <translation type="unfinished"></translation>
+        <translation>Kết thúc</translation>
     </message>
     <message>
         <location filename="../maintainwindow.ui" line="373"/>
         <source>Removal failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Việc gỡ cài đặt đã thất bại.</translation>
     </message>
     <message>
         <location filename="../maintainwindow.ui" line="398"/>
         <source>Exit</source>
-        <translation type="unfinished"></translation>
+        <translation>Thoát</translation>
     </message>
     <message>
         <location filename="../maintainwindow.ui" line="405"/>
         <source>Try Again</source>
-        <translation type="unfinished"></translation>
+        <translation>Thử lại</translation>
     </message>
     <message>
         <location filename="../maintainwindow.cpp" line="35"/>
         <source>Modify %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Thay đổi %1</translation>
     </message>
     <message>
         <location filename="../maintainwindow.cpp" line="36"/>
         <source>Are you sure you want to uninstall %1?</source>
-        <translation type="unfinished"></translation>
+        <translation>Bạn có chắc chắn muốn gỡ cài đặt của %1 không?</translation>
     </message>
     <message>
         <location filename="../maintainwindow.cpp" line="90"/>
         <source>%1 currently running</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 đang chạy</translation>
     </message>
     <message>
         <location filename="../maintainwindow.cpp" line="90"/>
         <source>Before we continue, you&apos;ll need to close %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>Trước khi tiếp tục được, bạn cần đóng %1.</translation>
     </message>
     <message>
         <location filename="../maintainwindow.cpp" line="111"/>
         <source>Getting ready to uninstall %1...</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang chuẩn bị gỡ cài đặt %1...</translation>
     </message>
 </context>
 <context>
@@ -266,7 +258,7 @@
     <message>
         <location filename="../quazip/quazipfile.cpp" line="247"/>
         <source>ZIP/UNZIP API error %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Lỗi API ZIP/UNZIP %1</translation>
     </message>
 </context>
 <context>
@@ -274,7 +266,7 @@
     <message>
         <location filename="../process/removeworker.cpp" line="57"/>
         <source>Removing %1...</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang gỡ cài đặt của %1...</translation>
     </message>
 </context>
 </TS>
