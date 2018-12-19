9cdc111e17b23ba3415af2050f0ebd3b5ba2574b
Victor Tran <vicr12345@gmail.com>
2018-12-20 00:04:31 +1100

    Fix build.bat

--- appveyor/build.bat
+++ appveyor/build.bat
@@ -4,11 +4,16 @@ if "%APPVEYOR_REPO_TAG_NAME%"=="continuous" (
 
 )
 
-echo Grabbing Qt 5.12 (Static)
+echo Grabbing required files
 curl -L http://downloads.sourceforge.net/project/theinstaller/Qt5.12.0-static.7z > Qt.7z
+curl -L https://sourceforge.net/projects/gnuwin32/files/zlib/1.2.3/zlib-1.2.3-lib.zip > zlib.zip
 "/Program Files/7-zip/7z.exe" x Qt.7z -oQtStatic -r
+"/Program Files/7-zip/7z.exe" x zlib.zip -ozlib -r
+
 
 set QTDIR=%cd%\QtStatic\Qt5.12.0-static
+set ZLIBINCLUDEDIR=%cd%\zlib\include
+set ZLIBDIR=%cd%\zlib\lib
 set PATH=%PATH%;%QTDIR%\bin
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 
@@ -27,12 +32,12 @@ set DEPLOY=%cd%\deploy
 echo %~2 > metadata.txt
 mkdir %~1
 pushd %~1
-qmake ..\theInstaller.pro
+qmake ..\theInstaller.pro "LIBS += -L\"%ZLIBDIR%\"" "INCLUDEPATH += \"%ZLIBINCLUDEDIR%\""
 nmake release
 copy installer\release\installer.exe %DEPLOY%\%~1.exe
 popd
 EXIT /B
 
 :buildversions
-CALL :build theSlate "http://vicr123.com/theslate/theinstaller/installer.json"
-CALL :build thePhoto "http://vicr123.com/thephoto/theinstaller/installer.json"
+CALL :build theSlate http://vicr123.com/theslate/theinstaller/installer.json
+CALL :build thePhoto http://vicr123.com/thephoto/theinstaller/installer.json
